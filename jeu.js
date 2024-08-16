bac3dfca31ba51240691c806a0685da20820fc09
katcita <katiuska.gc@gmail.com>
2024-08-16 23:20:25 +0200

    yes

--- jeu.js
+++ jeu.js
@@ -1,16 +1,16 @@
 function show1() {
   const b1 = document.getElementById('b1');
-  b1.innerHTML = 'perdu';
+  b1.innerHTML = '<img src="perdu.png" alt="">';
 }
 
 function show2() {
   const b2 = document.getElementById('b2');
-  b2.innerHTML = 'perdu';
+  b2.innerHTML = '<img src="perdu.png" alt="">';
 }
 
 function show3() {
   const b3 = document.getElementById("b3");
-  b3.innerHTML = "😻 😺"
+  b3.innerHTML = '<img src="gagner.png" alt="">'
 }
 
 function rejouer(){
